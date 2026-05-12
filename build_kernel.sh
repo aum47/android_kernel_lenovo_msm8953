78c582c96006148300c371545a116f954452220b
aum47 <aum.patel131108@gmail.com>
2026-05-12 08:49:14 +0530

    media: camera: fix msm_isp.h include path for trace header
    Add ISP directory to ccflags so msm_cam.h trace header can resolve
    the relative msm_isp.h include when compiled from outside the ISP
    directory.

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export ARCH=arm64
-export CROSS_COMPILE=/home/aumpatel/toolchain/gcc-linaro-7.5.0/bin/aarch64-linux-gnu-
+export CROSS_COMPILE=/home/aumpatel/toolchain/gcc-linaro-4.9/bin/aarch64-linux-gnu-
 
 # Fix yylloc double definition (idempotent)
 grep -q 'extern YYLTYPE yylloc' scripts/dtc/dtc-lexer.l || \
