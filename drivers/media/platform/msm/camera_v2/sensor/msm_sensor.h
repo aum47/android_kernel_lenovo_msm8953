36e3d3436ecfe8c0a977ac6c270be8fd500e9cf6
aum47 <aum.patel131108@gmail.com>
2026-05-12 20:14:30 +0530

    Add io/ so the files feel included

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
@@ -32,8 +32,8 @@
 #include <media/v4l2-subdev.h>
 #include <media/v4l2-ioctl.h>
 #include "io/msm_camera_i2c.h"
-#include "msm_camera_dt_util.h"
-#include "msm_sd.h"
+#include "io/msm_camera_dt_util.h"
+#include "io/msm_sd.h"
 
 #define DEFINE_MSM_MUTEX(mutexname) \
 	static struct mutex mutexname = __MUTEX_INITIALIZER(mutexname)
